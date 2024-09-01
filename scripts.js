6be0d46decb563d43fb47affa24053693f1fbaf2
23IT027 <23it027@charusat.edu.in>
2024-09-01 19:00:28 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Prime Video Clone Loaded');
+    // Additional JavaScript functionality can be added here.
+});
